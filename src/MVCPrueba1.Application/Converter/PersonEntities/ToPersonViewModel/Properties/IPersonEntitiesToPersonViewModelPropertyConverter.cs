85a6a7d7760660bfa2fcb79cd3bd10d4f37a4d5b
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-06-21 12:20:26 +0200

    Ajustado copyright

--- src/MVCPrueba1.Application/Converter/PersonEntities/ToPersonViewModel/Properties/IPersonEntitiesToPersonViewModelPropertyConverter.cs
+++ src/MVCPrueba1.Application/Converter/PersonEntities/ToPersonViewModel/Properties/IPersonEntitiesToPersonViewModelPropertyConverter.cs
@@ -1,5 +1,5 @@
 // <copyright file="IPersonEntitiesToPersonViewModelPropertyConverter.cs" company="Ricardo">
-// Copyright (c) Ricardo. All rights reserved.
+//     Copyright (c) Ricardo. All rights reserved.
 // </copyright>
 
 namespace MVCPrueba1.Application.Converter.PersonEntities.ToPersonViewModel.Properties
