9378ab874aa99cb5180518364a8cae4b85452384
Ricardo Vela Salas <rivesa77@hotmail.com>
2026-06-21 20:38:03 +0200

    Creado proyecto Application Tests

---
+++ src/MVCPrueba1.Application/Tests/UseCases/Persons/Creates/AddPersonUseCaseTests.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="AddPersonUseCaseTests.cs" company="Ricardo">
+//     Copyright (c) Ricardo. All rights reserved.
+// </copyright>
+
+namespace Application.Tests.UseCases.Persons.Creates
+{
+    [TestClass]
+    public class AddPersonUseCaseTests
+    {
+    }
+}
